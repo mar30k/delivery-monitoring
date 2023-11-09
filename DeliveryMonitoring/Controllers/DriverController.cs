9523cc14c5f0ff5859458640626bae907206b48c
CarmelZe <92999610+CarmelZe@users.noreply.github.com>
2023-11-09 20:39:36 +0300

    Update DriverController.cs

--- DeliveryMonitoring/Controllers/DriverController.cs
+++ DeliveryMonitoring/Controllers/DriverController.cs
@@ -11,12 +11,12 @@ namespace DeliveryMonitoring.Controllers
     {
         private readonly HttpClient _client;
 
-        Uri baseAddress = new Uri("http://196.189.21.67:8084/api");
+        Uri baseAddress = new Uri("uri_path");
         public DriverController(HttpClient client)
         {
             _client = client;
             _client.BaseAddress = baseAddress;
-            _client.DefaultRequestHeaders.Add("x-api-key", "c666e0e9-fnnm-5804-bbxo-144ad72ae730");
+            _client.DefaultRequestHeaders.Add("key", "api_key");
         }
 
         [HttpGet]
