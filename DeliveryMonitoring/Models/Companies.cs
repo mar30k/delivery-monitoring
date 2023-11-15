c28f02db553c551acfb76829982983a957e85be9
CarmelZe <carmel.zerihun@gmail.com>
2023-11-15 10:26:59 +0300

    new looks

--- DeliveryMonitoring/Models/Companies.cs
+++ DeliveryMonitoring/Models/Companies.cs
@@ -1,7 +1,10 @@
-﻿namespace DeliveryMonitoring.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeliveryMonitoring.Models
 {
     public class Companies
     {
+        [Display(Name="Company TIN")]
         public List<string> companyTins { get; set; }
     }
 }
