212e04221c4d421d215ee7bea8e38306f7bc62a7
CarmelZe <carmel.zerihun@gmail.com>
2023-12-01 09:49:15 +0300

    Login Added Successfully

---
+++ DeliveryMonitoring/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DeliveryMonitoring.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        [DataType(DataType.Text)]
+        [DisplayName("Username")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password)]
+        //[NoTrim]
+        [DisplayName("Password")]
+        public string? Password { get; set; }
+
+        [DisplayName("Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
