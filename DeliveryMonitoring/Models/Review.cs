8f7866270b284bf1d4869f264957727079be9621
Markos Beyene <markosbeyene1@gmail.com>
2025-06-17 16:09:24 +0300

    Add DriverReview and VerifyIdModel classes
    Introduced new classes `DriverReview` and `Reviews` in the `Review.cs` file to manage driver reviews and their details. Added `VerifyIdModel` and `EntityModel` classes in `VerifyIdModel.cs` for handling identification and entity-related data, enhancing the overall data structure and UI representation.

---
+++ DeliveryMonitoring/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿namespace DeliveryMonitoring.Models
+{
+    public class DriverReview
+    {
+        public int? Count { get; set; }
+        public decimal Rating { get; set; }
+       public List<Reviews>? Reviews { get; set; }
+    }
+
+    public class Reviews
+    {
+        public string? Image { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string ReviewerPhoneNumber { get; set; } = string.Empty;
+        public bool IsVerifiedUser { get; set; }
+        public string Review { get; set; } = string.Empty;
+        public string VoucherCode { get; set; } = string.Empty;
+        public string? Reply { get; set; }
+        public decimal Rating { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DeliveryMonitoring/Models/VerifyIdModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+
+namespace DeliveryMonitoring.Models
+{
+    public class VerifyIdModel
+    {
+        [DisplayName("Identification No.")]
+        public string? myId { get; set; }
+        [DisplayName("Remember")]
+        public bool remember { get; set; }
+    }
+    public class EntityModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int GslType { get; set; }
+        public string Tin { get; set; }
+        public string BioId { get; set; }
+        public string NationalId { get; set; }
+        public string PassportId { get; set; }
+        public bool IsPerson { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string Gender { get; set; }
+        public string BusinessType { get; set; }
+        public int Preference { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Nationality { get; set; }
+        public bool IsActive { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Note { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModified { get; set; }
+        public int MainConsigneeUnit { get; set; }
+        public string BaseUrl { get; set; }
+        public int? ParentId { get; set; }
+        public string Department { get; set; }
+        public string Branch { get; set; }
+        public string Position { get; set; }
+        public string CommunicationSource { get; set; }
+        public string DefaultLanguage { get; set; }
+        public string DefaultCurrency { get; set; }
+        public string DefaultImageUrl { get; set; }
+        public decimal? CreditLimit { get; set; }
+        public decimal? TransactionLimit { get; set; }
+        public bool Locked { get; set; }
+        public string Remark { get; set; }
+    }
+}
