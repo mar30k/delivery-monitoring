7a45352e647a6f8a0423d4c2450be083d6420177
Markos Beyene <markosbeyene1@gmail.com>
2025-06-17 16:08:02 +0300

    Add AlertsController and enhance layout for alerts
    Implemented AlertsController to manage alerts with a new route `/GetOrders` for fetching order details. Integrated dependency injection for HTTP client and context access.
    
    Updated `_Layout.cshtml` to retrieve `companyTin` from cookies, added CSS for a floating alert box, and included JavaScript for fetching and displaying alerts. The sidebar now conditionally shows a "Companies" link based on `companyTin`, and removed the old sidebar responsiveness script while integrating logout functionality.

---
+++ DeliveryMonitoring/Helpers/AlertsController.cs
@@ -0,0 +1,43 @@
+﻿using CNET_ERP_V7.WebConstants;
+using DeliveryMonitoring.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace DeliveryMonitoring.Helpers
+{
+    public class AlertsController : Controller
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AlertsController(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClientFactory = httpClientFactory;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [Route("/GetOrders")]
+        public async Task<List<OrderDetail>?> Index()
+        {
+            try
+            {
+                var response = new List<OrderDetail>();
+                var _client = _httpClientFactory.CreateClient("Delivery");
+                var companyTin = _httpContextAccessor.HttpContext?.Request.Cookies[CNET_WebConstantes.IdentificationCookie];
+
+                if (string.IsNullOrWhiteSpace(companyTin) || string.IsNullOrWhiteSpace(companyTin)) { return new List<OrderDetail>(); }
+
+                HttpResponseMessage responseMessage = await _client.GetAsync(_client.BaseAddress + $"/orderRequests?companyTin={companyTin}");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var responseMessageData = await responseMessage.Content.ReadAsStringAsync();
+                    response = JsonConvert.DeserializeObject<List<OrderDetail>>(responseMessageData);
+                }
+                return response ?? new List<OrderDetail>();
+            }
+            catch
+            {
+                return new List<OrderDetail>();
+            }
+        }
+    }
+}
