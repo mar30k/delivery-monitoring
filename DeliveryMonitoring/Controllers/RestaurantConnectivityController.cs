8c1a50bd6bf4b321dd60640cc3d969c84f48c116
Markos Beyene <markosbeyene1@gmail.com>
2025-06-17 16:03:20 +0300

    Add RestaurantConnectivityController and DeviceControl model
    Introduces the RestaurantConnectivityController to handle HTTP requests for restaurant connectivity, utilizing dependency injection for HTTP client and context access.
    
    Adds the DeviceControl model to represent device-related data with various properties.
    
    Updates the Index.cshtml view to display a table of DeviceControl objects, enhancing user experience with DataTable functionality for sorting and searching.

---
+++ DeliveryMonitoring/Controllers/RestaurantConnectivityController.cs
@@ -0,0 +1,52 @@
+﻿using CNET_ERP_V7.WebConstants;
+using DeliveryMonitoring.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace DeliveryMonitoring.Controllers
+{
+    [Authorize]
+    public class RestaurantConnectivityController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public RestaurantConnectivityController(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClientFactory = httpClientFactory;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [Route("/connectivity")]
+        public async Task<IActionResult >Index( string startDate, string endDate)
+        {
+            var client = _httpClientFactory.CreateClient("ApiBaseUrl");
+            var companyTin = _httpContextAccessor.HttpContext?.Request.Cookies[CNET_WebConstantes.IdentificationCookie];
+            try
+            {
+                var response = await client.GetAsync($"deviceControl?StartDate={startDate}&EndDate={endDate}");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    return View(null);
+                }
+
+                var responseData = await response.Content.ReadAsStringAsync();
+                var deviceControl = JsonConvert.DeserializeObject<List<DeviceControl>>(responseData);
+
+                if (companyTin != "0076217301")
+                {
+                    deviceControl = deviceControl?.Where(x => x?.Tin?.ToString() == companyTin?.Trim()).ToList();
+                }
+
+                return View(deviceControl);
+            }
+            catch (Exception ex)
+            {
+                return View(null);
+            }
+        }
+    }
+}
---
+++ DeliveryMonitoring/Models/DeviceControl.cs
@@ -0,0 +1,21 @@
+﻿namespace DeliveryMonitoring.Models
+{
+    public class DeviceControl
+    {
+
+        public string? Tin { get; init; }
+        public string? CompanyName { get; init; }
+        public string? BranchName { get; init; }
+        public string? DeviceName { get; init; }
+        public string? DeviceType { get; init; }
+        public string? MachineNo { get; init; }
+        public string? HostDevice { get; set; }
+        public string? UserName { get; init; }
+        public string? Note { get; init; }
+        public DateTime? TimeStamp { get; init; }
+        public bool? Seen { get; set; }
+        public string? Version { get; init; }
+        public string? Remark { get; init; }
+
+    }
+}
