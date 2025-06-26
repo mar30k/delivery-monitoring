4761663ad63662af6b932809fa1de57f29ba02f2
Markos Beyene <markosbeyene1@gmail.com>
2025-06-26 11:49:08 +0300

    Add Supervisors management with API integration
    Updated `SupervisorsController` to fetch and display a list of supervisors from an external API. Enhanced `Index.cshtml` to present supervisor details in a responsive table using DataTables, including necessary scripts for functionality.

---
+++ DeliveryMonitoring/Controllers/SupervisorsController.cs
@@ -0,0 +1,30 @@
+﻿using DeliveryMonitoring.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DeliveryMonitoring.Controllers
+{
+    [Authorize]
+    public class SupervisorsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public SupervisorsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("/supervisors")]
+        public async Task<IActionResult>Index()
+        {
+            var client = _httpClientFactory.CreateClient("CnetApiBaseUrl");
+            List<SupervisorsDTO> superVisors = new();
+            var supervisorTask = client.GetAsync($"{client.BaseAddress}auth/getsupervisors");           
+            if (supervisorTask.Result.IsSuccessStatusCode)
+            {
+                var data = await supervisorTask.Result.Content.ReadAsStringAsync();
+                superVisors = JsonConvert.DeserializeObject<List<SupervisorsDTO>>(data) ?? new List<SupervisorsDTO>();
+            }
+            return View(superVisors);
+        }
+    }
+}
