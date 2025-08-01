2a93afe33d9f522ce1febb9edc1e6f2b776f8125
Markos Beyene <markosbeyene1@gmail.com>
2025-08-01 14:24:13 +0300

    added CompletedOrders, TakeAwayOrders and DineInOrders in home view model

--- DeliveryMonitoring/Models/HomeViewModel.cs
+++ DeliveryMonitoring/Models/HomeViewModel.cs
@@ -3,6 +3,9 @@
     public class HomeViewModel
     {
         public List<Driver>? Drivers { get; set; }
+        public HulubejeResponse<List<CompletedOrders>>? CompletedOrders { get; set; }
+        public HulubejeResponse<List<CompletedOrders>>? TakeAwayOrders { get; set; }
+        public HulubejeResponse<List<CompletedOrders>>? DineInOrders { get; set; }
         public List<OrderDetail>? Orders { get; set; }
         public List<DeviceControl>? DeviceControl { get; set; }
         public List<SupervisorsDTO>? Supervisors { get; set; }
